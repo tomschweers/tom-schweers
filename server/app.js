b0a9dc33fdf0679f3c8deaec6c895c0d9273a407
tomschweers <trschweers@gmail.com>
2018-08-22 09:54:25 -0700

    Moved babelrc

--- server/app.js
+++ server/app.js
@@ -10,12 +10,13 @@ const users = require('./routes/users')
 
 const app = express()
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon-16.png')))
+// Favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -25,20 +26,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', index)
 app.use('/users', users)
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function (req, res, next) {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
-// error handler
+// Error handler
 app.use(function (err, req, res) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500)
   res.render('error')
 })
